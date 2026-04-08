97b2a0db8e4c3782153a148a288e20405ea23b9b
goodingr <goodingr@umich.edu>
2026-04-07 21:47:23 -0400

    gitignore

--- temp_archive/cleanup_20251120_180254/run_nba_metrics.py
+++
@@ -1,6 +0,0 @@
-import logging
-from src.data.sources.nba_rolling_metrics import ingest
-
-logging.basicConfig(level=logging.INFO)
-# Run for last few seasons to ensure we have data
-ingest(seasons=[2023, 2024, 2025])
